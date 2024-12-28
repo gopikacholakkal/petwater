0e0dd17c9d6d12b0ace57483dc7fc635edab6aaf
Gopika Gireesh <gopikagireesh9539@gmail.com>
2024-12-28 18:23:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log('sdfsd');
+//     const elements = document.querySelectorAll(".nav-item");
+
+//     elements.forEach(function(element) {
+//         element.addEventListener("click", function() {
+//             // Remove the "active" class from all elements
+//             elements.forEach(function(el) {
+//                 el.classList.remove("active");
+//             });
+              
+//             // Add the "active" class to the clicked element
+//             element.classList.add("active");
+//         });
+//     });
+// });
